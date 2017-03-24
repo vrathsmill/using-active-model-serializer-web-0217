7d7cc9a6391f73793e3105b7293b20f7a75520a5
vrathsmill <vlrathsmill@gmail.com>
2017-03-24 10:01:11 -0400

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,12 @@
 class AuthorsController < ApplicationController
+
   def show
-    @author = Author.find(params[:id])
+  @author = Author.find(params[:id])
+  respond_to do |f|
+    f.html { render :show }
+    f.json { render json: @author }
   end
+end
+
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,12 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
-    respond_to do |format|
-      format.html { render :show }
-      format.json { render json: @post.to_json(only: [:title, :description, :id],
-                              include: [author: { only: [:name]}]) }
-    end
-  end
+     @post = Post.find(params[:id])
+     respond_to do |format|
+       format.html { render :show }
+       format.json { render json: @post}
+     end
+   end
 
   def new
     @post = Post.new
